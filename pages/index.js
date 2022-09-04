0186e75e1c7b286d41919aa4da017e2fde1d7daa
abduh batubara <muhammadabdu089@gmai.com>
2022-09-04 14:44:07 +0700

    link use next/link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 function HomePage() {
-    return(
-        <div>Halaman home</div>
-    );
-};
+  return (
+    <div>
+      <p>Halaman Home</p>
+      <ul>
+        <li>
+            <Link href="/portfolio">Portofolio</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default HomePage;
