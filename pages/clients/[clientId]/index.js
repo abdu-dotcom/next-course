8b188efc7a868d4f5536f07fa79777a688fac76d
abduh batubara <muhammadabdu089@gmai.com>
2022-09-05 14:17:48 +0700

    65. Navigating Programmatically

--- pages/clients/[clientId]/index.js
+++ pages/clients/[clientId]/index.js
@@ -1,8 +1,20 @@
 import { useRouter } from "next/router";
 
 function ClientPage() {
-    const router = useRouter();
-    return <div>Halaman Client si : {router.query.clientId}</div>;
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "1" },
+    });
+  }
+  return (
+    <div>
+      <p>Halaman Client si : {router.query.clientId}</p>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 
 export default ClientPage;
